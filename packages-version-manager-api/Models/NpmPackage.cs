60c221494edb2593d6bb9d5a2761d60460600ba9
MauroRaya <maurorayabr@gmail.com>
2026-04-04 22:37:11 -0300

    feat: add npm registry package model

---
+++ packages-version-manager-api/Models/NpmPackage.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace packages_version_manager_api.Models
+{
+    public record NpmPackage
+    {
+        [JsonPropertyName("dist-tags")]
+        public Dictionary<string, string> DistTags { get; set; } = new();
+
+        [JsonPropertyName("time")]
+        public Dictionary<string, DateTime> Time { get; set; } = new();
+    }
+}
