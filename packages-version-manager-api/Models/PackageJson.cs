d8135afddacd49c987959c8125465ee051e68051
MauroRaya <maurorayabr@gmail.com>
2026-04-04 22:36:46 -0300

    feat: add package json model

---
+++ packages-version-manager-api/Models/PackageJson.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace packages_version_manager_api.Models
+{
+    public record PackageJson
+    {
+        [JsonPropertyName("dependencies")]
+        public Dictionary<string, string> Dependencies { get; set; } = new();
+    }
+}
