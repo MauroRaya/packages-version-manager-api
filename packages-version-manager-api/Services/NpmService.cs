73d7aff86f8cb8cf33fd5e8457572c96ee601029
MauroRaya <maurorayabr@gmail.com>
2026-04-04 22:38:02 -0300

    feat: add npm service

---
+++ packages-version-manager-api/Services/NpmService.cs
@@ -0,0 +1,46 @@
+﻿using packages_version_manager_api.Models;
+
+namespace packages_version_manager_api.Services
+{
+    public class NpmService
+    {
+        private readonly GithubService _githubService;
+
+        public NpmService(GithubService githubService)
+        {
+            _githubService = githubService;
+        }
+
+        public async Task<string> GetPackageLatestVersion(
+            string packageName, 
+            CancellationToken token)
+        {
+            using HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync($"https://registry.npmjs.org/{packageName}", token);
+            response.EnsureSuccessStatusCode();
+
+            NpmPackage? package = await response.Content.ReadFromJsonAsync<NpmPackage>(token);
+            if (package is null) throw new Exception();
+
+            return package.DistTags["latest"];
+        }
+
+        public async Task<Dictionary<string, string>> GetPackages(
+            string username,
+            string repository,
+            string branch,
+            CancellationToken token)
+        {
+            string filePath = "package.json";
+
+            HttpResponseMessage response = await _githubService.GetPackageManagerFile(username, repository, branch, filePath, token);
+            response.EnsureSuccessStatusCode();
+
+            PackageJson? packageJson = await response.Content.ReadFromJsonAsync<PackageJson>(token);
+            if (packageJson is null) throw new Exception();
+
+            return packageJson.Dependencies;
+        }
+    }
+}
