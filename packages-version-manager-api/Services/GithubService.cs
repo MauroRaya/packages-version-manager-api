18f903e5d0933ff8246f0597a80a0bb749a34dde
MauroRaya <maurorayabr@gmail.com>
2026-04-04 22:37:43 -0300

    feat: add github service using raw content api for fetching package manager file like package.json

---
+++ packages-version-manager-api/Services/GithubService.cs
@@ -0,0 +1,17 @@
+﻿namespace packages_version_manager_api.Services
+{
+    public class GithubService
+    {
+        public async Task<HttpResponseMessage> GetRawFile(
+            string username,
+            string repository,
+            string branch,
+            string filePath,
+            CancellationToken token)
+        {
+            using HttpClient client = new HttpClient();
+            string url = "https://raw.githubusercontent.com";
+            return await client.GetAsync($"{url}/{username}/{repository}/refs/heads/{branch}/{filePath}", token);
+        }
+    }
+}
